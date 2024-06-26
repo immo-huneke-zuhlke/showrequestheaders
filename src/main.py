52b478e4a3d714d1a68eb5181cbe0ebc88bb25dd
immo.h.huneke <immo.h.huneke@gmail.com>
2024-06-26 15:34:52 +0000

    first commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 80)))
+
+if __name__ == "__main__":
+    main()
